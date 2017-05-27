f2847ad4838dd0420f4ec0249ac294ec01ff1c2b
scarletrunner7000 <scarletrunner7000@gmail.com>
2017-05-27 12:49:06 +0900

    move publish code under server/

--- imports/api/tasks.js
+++ imports/api/tasks.js
@@ -4,18 +4,7 @@ import { check } from "meteor/check";
 
 export const Tasks = new Mongo.Collection('tasks');
 
-if (Meteor.isServer) {
-  // This code only runs on the server
-  // Only publish tasks that are public or belong to the current user
-  Meteor.publish('tasks', function tasksPublication () {
-    return Tasks.find({
-      $or: [
-        { private: { $ne: true } },
-        { owner: this.userId },
-      ]
-    });
-  });
-}
+
 
 Meteor.methods({
   'tasks.insert'(text) {
@@ -36,7 +25,7 @@ Meteor.methods({
   'tasks.remove'(taskId) {
     check(taskId, String);
 
-    const task = Task.findOne(taskId);
+    const task = Tasks.findOne(taskId);
     if (task.private && task.owner !== Meteor.userId()) {
       // If the task is private, make sure only the owner can check it off
       throw new Meteor.Error('not-authorized');
--- server/main.js
+++ server/main.js
@@ -1,6 +1,18 @@
 import { Meteor } from 'meteor/meteor';
-import "../imports/api/tasks.js"
+import { Tasks } from "../imports/api/tasks.js"
 
 Meteor.startup(() => {
   // code to run on server at startup
+  if (Meteor.isServer) {
+    // This code only runs on the server
+    // Only publish tasks that are public or belong to the current user
+    Meteor.publish('tasks', function tasksPublication () {
+      return Tasks.find({
+        $or: [
+          { private: { $ne: true } },
+          { owner: this.userId },
+        ]
+      });
+    });
+  }
 });
