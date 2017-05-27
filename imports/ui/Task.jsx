2a5588c657964f465904bc7c5fa11ec5ff1d2f28
scarletrunner7000 <scarletrunner7000@gmail.com>
2017-05-27 09:31:35 +0900

    finish 8. Adding user accounts

--- imports/ui/Task.jsx
+++ imports/ui/Task.jsx
@@ -30,10 +30,11 @@ export default class Task extends Component {
           type="checkBox"
           readOnly
           checked={this.props.task.checked}
-          onClick={this.toggleChecked.bind(this)}
-        />
+          onClick={this.toggleChecked.bind(this)} />
 
-        <span className="text">{this.props.task.text}</span>
+        <span className="text">
+          <strong>{this.props.task.username}</strong>: {this.props.task.text}
+        </span>
       </li>
     );
   }
